a7983e8cc0f0c4d0cdd8d60031ee9c2471efb152
Priya Ranjan Singh <mail@priyaranjan.net>
2018-04-27 14:49:28 +0200

    refactor app to add city as a variable

--- app.js
+++ app.js
@@ -1,30 +1,23 @@
 const request = require('request');
 
-const url1 = 'https://api.foursquare.com/v2/venues/search?near=Berlin&intent=Browse&categoryId=4d4b7104d754a06370d81259&oauth_token=0NFKW43DRFDMQAIQLKSAMW5PM1MLEL5UI2AD41WO14I35DP1&v=20180406&_=1523029187756';
-const url2 = 'https://api.foursquare.com/v2/venues/search?near=Berlin&intent=Browse&categoryId=4d4b7105d754a06373d81259&oauth_token=0NFKW43DRFDMQAIQLKSAMW5PM1MLEL5UI2AD41WO14I35DP1&v=20180406&_=1523029187756';
-const url3 = 'https://api.foursquare.com/v2/venues/search?near=Berlin&intent=Browse&categoryId=4d4b7105d754a06374d81259&oauth_token=0NFKW43DRFDMQAIQLKSAMW5PM1MLEL5UI2AD41WO14I35DP1&v=20180406&_=1523029187756';
-const url4 = 'https://api.foursquare.com/v2/venues/search?near=Berlin&intent=Browse&categoryId=4d4b7105d754a06376d81259&oauth_token=0NFKW43DRFDMQAIQLKSAMW5PM1MLEL5UI2AD41WO14I35DP1&v=20180406&_=1523029187756';
-const url5 = 'https://api.foursquare.com/v2/venues/search?near=Berlin&intent=Browse&categoryId=4d4b7105d754a06377d81259&oauth_token=0NFKW43DRFDMQAIQLKSAMW5PM1MLEL5UI2AD41WO14I35DP1&v=20180406&_=1523029187756';
+const apiRoot = 'https://api.foursquare.com/v2/venues/search';
+const token = '0NFKW43DRFDMQAIQLKSAMW5PM1MLEL5UI2AD41WO14I35DP1';
+const city = 'Berlin';
+const categories = ['4d4b7104d754a06370d81259', '4d4b7105d754a06373d81259',
+  '4d4b7105d754a06374d81259', '4d4b7105d754a06376d81259',
+  '4d4b7105d754a06377d81259'];
 
-apiRequest(url1, function (venue) {
-  console.log(venue);
+categories.map(function(categoryId, index) {
+  const url = getUrl(categories[index]);
+  apiRequest(url, function(venue) {
+    console.log(venue);
+  })
 });
 
-apiRequest(url2, function (venue) {
-  console.log(venue);
-});
-
-apiRequest(url3, function (venue) {
-  console.log(venue);
-});
-
-apiRequest(url4, function (venue) {
-  console.log(venue);
-});
-
-apiRequest(url5, function (venue) {
-  console.log(venue);
-});
+function getUrl(categoryId) {
+  return `${apiRoot}?near=${city}&intent=Browse&categoryId=${categoryId}
+    &oauth_token=${token}&v=20180406`;
+}
 
 function apiRequest(url, callback) {
   request.get(url, function (error, response, body) {
